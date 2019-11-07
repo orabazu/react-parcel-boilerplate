52f274293808326d59955552bff63a29b39fb33d
zhunor <ozerorhun@gmail.com>
2019-11-07 23:27:59 +0300

    Add scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
+import './style.scss';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <header>oh header</header>
+        <h1 className="heading">Build with <span role="img" aria-label="emoji"> 📦</span></h1>
       </div>
     );
   }
